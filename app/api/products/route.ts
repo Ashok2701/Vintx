7aafbed1657827a03848adf3237401c9784f94ad
Ashok2701 <61420638+Ashok2701@users.noreply.github.com>
2025-06-21 20:18:02 +0530

    Updated route.ts

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
-
 import { auth } from "@clerk/nextjs/server";
 
 export async function POST(req: Request) {
@@ -8,12 +7,11 @@ export async function POST(req: Request) {
     const { name, description, price, categoryId, images, sellerId } = await req.json();
 
     // Optional: Server-side auth check (if needed)
-    const { userId  } = await auth();
+    const { userId } = await auth();
   
-console.log("at product seller param", sellerId)
-//const user = await prisma.user.findUnique({ where: { userId } });
-
-console.log("at product api", userId)
+    console.log("at product seller param", sellerId);
+    console.log("at product api", userId);
+    
     if (!userId && !sellerId) {
       return NextResponse.json({ error: "You must be signed in to create a product." }, { status: 401 });
     }
@@ -23,7 +21,6 @@ console.log("at product api", userId)
     }
 
     const product = await prisma.product.create({
-     
       data: {
         name,
         description,
@@ -39,20 +36,29 @@ console.log("at product api", userId)
     console.error("Product creation failed:", err);
     return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
-
-
 }
 
 export async function GET() {
   try {
     const products = await prisma.product.findMany({
-      orderBy: { createdAt: "desc" }
-    
+      include: {
+        category: {
+          select: {
+            name: true,
+          },
+        },
+        seller: {
+          select: {
+            name: true,
+          },
+        },
+      },
+      orderBy: { createdAt: "desc" },
     });
 
     return NextResponse.json(products);
   } catch (error) {
     console.error("Product fetch failed:", error);
-    return new NextResponse("Internal Server Error", { status: 500 });
+    return NextResponse.json({ error: "Failed to fetch products" }, { status: 500 });
   }
 }
